b88a4e2aa6c1a6f8f582c0b6a6ee87863bfead9b
leehyunkee <brdn2925@gmail.com>
2022-06-29 21:28:07 +0900

    학술회 끝 6/29

--- programing study 0628/programing study 0628/programing1.cpp
+++ programing study 0628/programing study 0628/programing1.cpp
@@ -3,7 +3,13 @@
 
 int main()
 {
-	printf("Hello World!");
-
+	printf("4 + 8 + 6 = %d", 4+8+6);
+	
 	return 0;
 }
+
+//int = 정수 = %d
+//char = 문자 = %d, %c, %s
+//short = 문자 하나 ex) e = %c
+//long = 문자열 ex) chocolate = %s
+//float = 소수까지
